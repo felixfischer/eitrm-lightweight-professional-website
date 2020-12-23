61e6caeda4e2c1918ab2ee2c0777f977d92d8906
Felix Fischer <felix@subtract.de>
2020-12-23 17:23:21 +0100

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,30 @@ module.exports = {
         './src/template/*',
     ],
     theme: {
+        colors: {
+            blue: {
+                DEFAULT: '#004494'
+            },
+            teal: {
+                DEFAULT: '#009E83',
+                light: '#D0EDE8',
+                lighter: '#DAF1ED'
+            },
+            grey: {
+                DEFAULT: '#57585A'
+            },
+            black: '#000000',
+            white: '#ffffff'
+        },
+        fontSize: {
+            sm: '1rem',      // 16px
+            base: '1.125rem',   // 18px
+            lg: '1.5rem',       // 24px
+            xl: '1.75rem',      // 28px
+            '2xl': '2.25rem',   // 36px
+            '3xl': '3.4375rem', // 55px
+            '4xl': '4.125rem',  // 66px
+        },
         extend: {},
     },
     variants: {},
