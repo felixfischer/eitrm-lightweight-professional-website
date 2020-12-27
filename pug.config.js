9e7efc3814a05e7117c9403c75d457ae96aeeb2a
Felix Fischer <felix@subtract.de>
2020-12-27 16:07:02 +0100

    Include collections data in Pug locals

--- pug.config.js
+++ pug.config.js
@@ -1,7 +1,22 @@
 const fs = require('fs')
 const jsYaml = require('js-yaml')
+const readdir = require("readdir-enhanced")
  
+const collectionsDir = './src/collections'
+
 const data = jsYaml.safeLoad(fs.readFileSync('./src/data/main.yml', 'utf8'))
+data.collections = {}
+
+let collectionsFiles = readdir.sync(collectionsDir, { filter: '**/*.yml', deep: 1 })
+collectionsFiles.forEach(relativePath => {
+  let collectionName = relativePath.split('/')[0]
+  let obj = jsYaml.safeLoad(fs.readFileSync(collectionsDir + '/' + relativePath, 'utf8'))
+  if (!Object.keys(data.collections).includes(collectionName)) {
+    data.collections[collectionName] = []
+  }
+  data.collections[collectionName].push(obj)
+})
+console.log(data)
 
 module.exports = {
   locals: data
