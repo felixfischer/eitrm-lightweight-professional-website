b714758ee2fee3ec4b2c25627d57198dd539c2c5
Felix Fischer <felix@subtract.de>
2020-12-27 22:17:27 +0100

    Development Snapshot

--- pug.config.js
+++ pug.config.js
@@ -1,39 +1,42 @@
 const fs = require('fs')
 const jsYaml = require('js-yaml')
-const readdir = require("readdir-enhanced")
-const indexBy = require('lodash.indexby')
-const map = require('lodash.map')
-const findWhere = require('lodash.findwhere')
+const readdir = require('readdir-enhanced')
+const _ = require('lodash')
  
+const dataFile = './src/data/main.yml'
 const collectionsDir = './src/collections'
 
-const data = jsYaml.safeLoad(fs.readFileSync('./src/data/main.yml', 'utf8'))
-data.collections = {}
-
-let collectionsFiles = readdir.sync(collectionsDir, { filter: '**/*.yml', deep: 1 })
-collectionsFiles.forEach(relativePath => {
-  let collectionName = relativePath.split('/')[0]
-  let obj = jsYaml.safeLoad(fs.readFileSync(collectionsDir + '/' + relativePath, 'utf8'))
-  if (!Object.keys(data.collections).includes(collectionName)) {
-    data.collections[collectionName] = []
-  }
-  data.collections[collectionName].push(obj)
-})
+function loadCollections() {
+  let collectionsFiles = readdir.sync(collectionsDir, { filter: '**/*.yml', deep: 1 })
+  let collections = {}
+  collectionsFiles.forEach(relativePath => {
+    let collectionName = relativePath.split('/')[0]
+    let obj = jsYaml.safeLoad(fs.readFileSync(collectionsDir + '/' + relativePath, 'utf8'))
+    if (!Object.keys(collections).includes(collectionName)) {
+      collections[collectionName] = []
+    }
+    collections[collectionName].push(obj)
+  })
+  Object.keys(collections).forEach(function (collName) {
+    collections[collName] = _.sortBy(collections[collName], 'order')
+  })
+  return collections
+}
 
 function includeRelatedItems(dataObj, parentCollection, relatedCollection) {
   let collectionItems = dataObj[parentCollection]
-  collectionItems = map(collectionItems, (item) => {
-    // each of parentCollection
-    if (Object.keys(item).includes(relatedCollection)) {
-      // parentCollection has a key for relatedCollection
-      item[relatedCollection + '_data'] = map(dataObj[relatedCollection], findWhere(dataObj[relatedCollection], {title: item[relatedCollection]}))
-    }
+  collectionItems = _.map(collectionItems, (item) => {
+    // map each item of parentCollection (e.g., 'level 1')
+    item[relatedCollection] = _.filter(dataObj[relatedCollection], {level: item.title})
     return item
   })
 }
 
+let data = jsYaml.safeLoad(fs.readFileSync(dataFile, 'utf8'))
+data.collections = loadCollections()
 includeRelatedItems(data.collections, 'levels', 'modules')
-//console.log(data.collections.levels)
+
+data.marked = require('marked')
 
 module.exports = {
   locals: data
