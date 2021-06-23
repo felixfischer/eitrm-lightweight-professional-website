413a4043314d837a71d558f7ab6e3ed80f598212
Felix Fischer <felix@subtract.de>
2021-06-23 15:18:15 +0200

    Development snapshot

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,5 @@
 import 'alpinejs'
+import $ from 'jquery'
 
 //const data = require('../data/main.yml')
 //(data)
@@ -15,18 +16,56 @@ function main() {
     // Ready
     // console.debug('ready')
     stopBodyFromScrollingWhenModalIsOpen()
+    handleFormSubmission('#lightprof-application-form')
 }
 
 function stopBodyFromScrollingWhenModalIsOpen() {
-    /* when modal is opened */
-    document.querySelector(".modal-opener").addEventListener('click', function() {
-        //document.querySelector("#modal-container").style.display = 'block'
-        document.querySelector("body").style.overflow = 'hidden'
-    })
-    /* when modal is closed */
-    document.querySelector(".modal-closer").addEventListener('click', function() {
-        //document.querySelector("#modal-container").style.display = 'none'
-        document.querySelector("body").style.overflow = 'visible'
+    if (document.querySelector(".modal-opener")) {
+        /* when modal is opened */
+        document.querySelector(".modal-opener").addEventListener('click', function() {
+            //document.querySelector("#modal-container").style.display = 'block'
+            document.querySelector("body").style.overflow = 'hidden'
+        })
+    }
+    if (document.querySelector(".modal-closer")) {
+        /* when modal is closed */
+        document.querySelector(".modal-closer").addEventListener('click', function() {
+            //document.querySelector("#modal-container").style.display = 'none'
+            document.querySelector("body").style.overflow = 'visible'
+        })
+    }
+}
+
+function handleFormSubmission(formId) {
+    var frm = $(formId)
+    frm.submit(function (e) {
+        e.preventDefault()
+        $(formId + ' .ajax-loader').show()
+        $(formId + ' .hide-on-submit').hide()
+        var formData = new FormData(frm[0]) 
+        $.ajax({
+            type: frm.attr('method'),
+            url: frm.attr('action'),
+            dataType: 'text',  // what to expect back from the PHP script, if anything
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: formData,
+            complete: function () {
+                $(formId + ' .ajax-loader').hide()
+            },
+            success: function (retData) {
+                //console.log('Submission was successful.')
+                console.log(retData)
+                $(formId + ' .submit-success').show()
+            },
+            error: function (retData) {
+                //console.log('An error occurred.')
+                console.log(retData)
+                $(formId + ' .hide-on-submit').show()
+                $(formId + ' .submit-error').show()
+            },
+        })
     })
 }
 
